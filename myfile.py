61105b71a51d67c81b37a4da83f299b79769c2c8
Nayan Taori <64462883+NDT2000@users.noreply.github.com>
2024-08-25 17:23:30 +0000

    Code enhanced

--- myfile.py
+++ myfile.py
@@ -1,15 +1,12 @@
-def Fibonacci(n):
-
-	if n < 0:
-		print("Incorrect input")
-
-	elif n == 0:
-		return 0
-
-	elif n == 1 or n == 2:
-		return 1
-
-	else:
-		return Fibonacci(n-1) + Fibonacci(n-2)
-
-print(Fibonacci(9))
+n = 9
+num1 = 0
+num2 = 1
+next_number = num2  
+count = 1
+ 
+while count <= n:
+    print(next_number, end=" ")
+    count += 1
+    num1, num2 = num2, next_number
+    next_number = num1 + num2
+print()
