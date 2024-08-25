d7389a43a84efb9105576d13e1f01a8d940e0565
Nayan Taori <64462883+NDT2000@users.noreply.github.com>
2024-08-25 18:33:36 +0000

    Adding input functionality

--- myfile.py
+++ myfile.py
@@ -1,12 +1,16 @@
-n = 9
-number1 = 0
-number2 = 1
-sum = number2  
-count = 1
- 
-while count <= n:
-    print(sum, end=" ")
-    count += 1
-    number1, number2 = number2, sum
-    sum = number1 + number2
-print()
+def fibonacci(n):
+    number1 = 0
+    number2 = 1
+    sum = number2  
+    count = 1
+	
+    while count <= n:
+        print(sum, end=" ")
+        count += 1
+        number1, number2 = number2, sum
+        sum = number1 + number2
+	
+    return sum
+
+n = int(input())
+print(fibonacci(n))
