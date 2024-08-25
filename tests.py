d1c20180cb5cf93c0943f5ded33e4447f1459779
LavanyaMiddha <49492261+LavanyaMiddha@users.noreply.github.com>
2024-08-25 19:47:44 +0000

    added a failing test case

--- tests.py
+++ tests.py
@@ -2,5 +2,9 @@
 import pytest
 from myfile import fibonacci
 
-def test_correct():
+def test_fibonacci():
   assert fibonacci(5) == 5
+
+def test_incorrect_fibonnaci():
+  with pytest.raises(ValueError):
+    fibonacci(-1) 
