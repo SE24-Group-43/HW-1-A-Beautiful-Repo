f1ed9949a72470df5a0feb183c32e3b2c24bcb9c
Nayan Taori <64462883+NDT2000@users.noreply.github.com>
2024-08-28 16:51:25 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,6 +5,9 @@ from myfile import fibonacci
 def test_fibonacci():
   assert fibonacci(5) == 5
 
-def test_incorrect_fibonnaci():
-  with pytest.raises(ValueError):
-    fibonacci(-1) 
+def test_fibonacci1():
+  assert fibonacci(-1) == 0
+
+# def test_incorrect_fibonnaci():
+#   with pytest.raises(ValueError):
+#     fibonacci(-1) 
